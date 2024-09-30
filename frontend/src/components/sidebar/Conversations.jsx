c83c6b31ff5814c339f0a15703a5fd0a1feb0ed0
Ahamad Tanzeel Khan <ahamadtanzeel86@gmail.com>
2024-09-30 16:30:45 +0530

    2nd part completed

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,16 @@
+// import useGetConversations from "../../hooks/useGetConversations";
+// import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+		</div>
+	);
+};
+export default Conversations;
