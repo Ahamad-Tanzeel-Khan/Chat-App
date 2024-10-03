1eb80eb37b948b4e49ea18380906d5a94a57a9c0
Ahamad Tanzeel Khan <ahamadtanzeel86@gmail.com>
2024-10-03 15:11:46 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToDatabase from "./db/connectToDatabase.js";
 import { app, server } from "./socket/socket.js";
+import path from 'path';
 
 
 dotenv.config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World!")
-// });
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+});
 
 server.listen(PORT, () => {
     connectToDatabase();
